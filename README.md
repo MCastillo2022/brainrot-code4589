30dd9653c87fc0ea79341cbdef47f75d73c65ed0
MCastillo2022 <101064185+MCastillo2022@users.noreply.github.com>
2025-05-30 12:36:52 -0700

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://mcastillo2022.github.io/brainrot-code4589/](https://mcastillo2022.github.io/brainrot-code4589/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/mcastillo2022/brainrot-code4589** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/mcastillo2022/brainrot-code4589** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "skibidiToilet":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 1 1 1 1 . . . . 
+. . . . . e e e 1 1 1 1 1 . . . 
+. . . . d d e e e 1 1 1 1 1 . . 
+. . . d d f d e e 1 1 f 1 1 . . 
+. . . . d d d d e 1 1 f f 1 . . 
+. . . . d d d d d 1 1 1 1 1 . . 
+. . 1 1 d d d d 6 6 1 1 1 1 . . 
+. 1 1 6 6 6 d d 6 6 6 1 1 1 . . 
+. 1 6 6 6 6 6 6 6 6 6 1 1 1 . . 
+. 1 1 1 6 6 6 6 6 6 1 1 1 1 . . 
+. . . 1 1 1 1 1 1 1 1 1 1 . . . 
+. . . . 1 1 1 1 1 1 1 1 1 . . . 
+. . . . . 1 1 1 1 1 1 1 1 . . . 
+. . . . . . . 1 1 1 1 1 . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
